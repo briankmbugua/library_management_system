a04ce697c1458375debc7e0ba6eaa41070e7d280
briankmbugua <briankmbuguak@gmail.com>
2023-09-03 17:34:41 +0300

    did database init, create roots to add users and subjects

--- application.py
+++ application.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, jsonify, request, redirect, flash, url_for, session
-
-from models import db, usersList, Subjects, IssueReturn, bookMaster
-
+from flask import Flask, render_template, request, url_for, session
+from models import *
 from datetime import date, timedelta
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost:3306/libraryDB"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost:3306/libdb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = 'my_secret_key'
+app.secret_key = b'hkahs3720/'
 db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -16,9 +17,39 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/logout")
-def logout():
-    return render_template("index.html")
+@app.route("/addUser", methods=("POST", "GET"))
+def addUser():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        usertype = request.form['userType']
+
+        fakeuser = [username, password, usertype]
+
+        user = usersList(userName=username,
+                         password=password, userType=usertype)
+        db.session.add(user)
+        db.session.commit()
+
+        return render_template("index.html", user=fakeuser)
+    elif request.method == "GET":
+        return render_template("addUser.html")
+
+
+@app.route("/addSubject", methods=("POST", "GET"))
+def addSubject():
+    if request.method == "POST":
+        subject = request.form['subject']
+
+        fakesubject = [subject]
+
+        subject = Subjects(subName=subject)
+        db.session.add(subject)
+        db.session.commit()
+
+        return render_template("index.html", subject=fakesubject)
+    elif request.method == "GET":
+        return render_template("addSubject.html")
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,42 +1,44 @@
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-# subjects model
 class Subjects(db.Model):
     __tablename__ = "Subjects"
     subID = db.Column(db.Integer, primary_key=True)
-    subName = db.Column(db.String, nullable=False)
+    subName = db.Column(db.String(50), nullable=False)
 
 
 class usersList(db.Model):
     __tablename__ = "users"
     userID = db.Column(db.Integer, primary_key=True)
-    userName = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    userType = db.Column(db.String, nullable=False)
+    userName = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    userType = db.Column(db.String(50), nullable=False)
 
 
 class bookMaster(db.Model):
     __tablename__ = "bookMaster"
-    accNumber = db.Column(db.String, primary_key=True)
-    bookTitle = db.Column(db.String, nullable=False)
-    SubID = db.Column(db.Integer, db.ForeignKey("Subjects.subID"))
-    authorName = db.Column(db.String, nullable=False)
-    publisherName = db.Column(db.String, nullable=False)
-    pages = db.Column(db.String, nullable=False)
-    price = db.Column(db.String, nullable=False)
-    status = db.Column(db.String, nullable=False)
+    accNumber = db.Column(db.String(50), primary_key=True)
+    bookTitle = db.Column(db.String(50), nullable=False)
+    SubID = db.Column(db.Integer, db.ForeignKey(
+        "Subjects.subID"), nullable=False)
+    authorName = db.Column(db.String(50), nullable=False)
+    PublisherName = db.Column(db.String(50), nullable=False)
+    pages = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer)
+    status = db.Column(db.String(50))
     subject = db.relationship("Subjects", backref="subject", lazy=True)
 
 
 class IssueReturn(db.Model):
     __tablename__ = "IssueReturn"
     transID = db.Column(db.Integer, primary_key=True)
-    AccNumber = db.Column(db.String, db.ForeignKey(
+    AccNumber = db.Column(db.String(50), db.ForeignKey(
         "bookMaster.accNumber"), nullable=False)
     userID = db.Column(db.Integer, db.ForeignKey(
-        "users.subID"), nullable=False)
+        "users.userID"), nullable=False)
     IssueDate = db.Column(db.Date, nullable=False)
     ExpRetDate = db.Column(db.Date, nullable=False)
     ActRetDate = db.Column(db.Date, nullable=False)
