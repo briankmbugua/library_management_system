438ad78ec5b9a32c73390c8523b1b35e94b557d7
briankmbugua <briankmbuguak@gmail.com>
2023-09-19 13:34:42 +0300

    added the library table and associated it with the users table, bookMaster table, issueReturn table

--- models.py
+++ models.py
@@ -3,6 +3,12 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 
+class Library(db.Model):
+    __tablename__ = "libraries"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
 class Subjects(db.Model):
     __tablename__ = "Subjects"
     subID = db.Column(db.Integer, primary_key=True)
@@ -12,9 +18,12 @@ class Subjects(db.Model):
 class usersList(db.Model, UserMixin):
     __tablename__ = "users"
     userID = db.Column(db.Integer, primary_key=True)
+    library_id = db.Column(db.Integer, db.ForeignKey(
+        "libraries.id"), nullable=False)
     userName = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(256), nullable=False)
     userType = db.Column(db.String(50), nullable=True)
+    library = db.relationship("libraries", backref="library", lazy=True)
 
     def get_id(self):
         return str(self.userID)
@@ -26,12 +35,15 @@ class bookMaster(db.Model):
     bookTitle = db.Column(db.String(50), nullable=False)
     SubID = db.Column(db.Integer, db.ForeignKey(
         "Subjects.subID"), nullable=True)
+    library_id = db.Column(db.Integer, db.ForeignKey(
+        "libraries.id"), nullable=False)
     authorName = db.Column(db.String(50), nullable=False)
     PublisherName = db.Column(db.String(50), nullable=False)
     pages = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Integer)
     status = db.Column(db.String(50))
     subject = db.relationship("Subjects", backref="subject", lazy=True)
+    library = db.relationship("libraries", backref="library", lazy=True)
 
 
 class IssueReturn(db.Model):
@@ -41,9 +53,12 @@ class IssueReturn(db.Model):
         "bookMaster.accNumber"), nullable=False)
     userID = db.Column(db.Integer, db.ForeignKey(
         "users.userID"), nullable=False)
+    library_id = db.Column(db.Integer, db.ForeignKey(
+        "libraries.id"), nullable=False)
     IssueDate = db.Column(db.Date, nullable=False)
     ExpRetDate = db.Column(db.Date, nullable=False)
     ActRetDate = db.Column(db.Date, nullable=False)
     OverdueDays = db.Column(db.Integer, nullable=False)
     book = db.relationship("bookMaster", backref="book", lazy=True)
     usr = db.relationship("usersList", backref="user", lazy=True)
+    library = db.relationship("libraries", backref="library", lazy=True)
